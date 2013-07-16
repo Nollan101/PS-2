b115401220cd0198e07b9a05e28d84ab918a3260
Oiawesome <Oiawesome99@yahoo.com>
2013-07-16 14:41:58 -0700

    add msot of teh stuff, excluding some LOU mons which I will add later

--- mods/pokecybertiers/formats-data.js
+++ mods/pokecybertiers/formats-data.js
@@ -1,1 +1,82 @@
-///ffff//
+exports.BattleFormatsData = {
+        chandelure: {
+                inherit: true,
+                tier: 'Uber'
+        },
+        thundurus: {
+                inherit: true,
+                tier: 'OU'
+        },
+        tornadust: {
+                inherit: true,
+                tier: 'OU'
+        },
+        landorus: {
+                inherit: true,
+                tier: 'Uber'
+        },
+        serperior: {
+                inherit: true,
+                tier: 'UU'
+        },
+        sawsbuck: {
+                inherit: true,
+                tier: 'UU'
+        },
+        ferroseed: {
+                inherit: true,
+                tier: 'UU'
+        },
+        entei: {
+                inherit: true,
+                tier: 'UU'
+        },
+        vaporeon: {
+                inherit: true,
+                tier: 'UU'
+        },
+        toxicroak: {
+                inherit: true,
+                tier: 'RU'
+        },
+        kabutops: {
+                inherit: true,
+                tier: 'BL2'
+        },
+        cobalion: {
+                inherit: true,
+                tier: 'RU'
+        },
+        scyther: {
+                inherit: true,
+                tier: 'NU'
+        },
+        hippopotas: {
+                inherit: true,
+                tier: 'NU'
+        },
+        politoed: {
+                inherit: true,
+                tier: 'LOU'
+        },
+        unknown: {
+                inherit: true,
+                tier: 'LOU'
+        },
+        womadam: {
+                inherit: true,
+                tier: 'LOU'
+        },
+        womadamtrash: {
+                inherit: true,
+                tier: 'LOU'
+        },
+        womadamsandy: {
+                inherit: true,
+                tier: 'LOU'
+        },
+        swalot: {
+                inherit: true,
+                tier: 'LOU'
+        },          
+};
