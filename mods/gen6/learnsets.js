5699b7f7f8bb64b2b53ded02eb520a75d81b327d
Oiawesome <Oiawesome99@yahoo.com>
2013-07-17 15:51:54 -0700

    Update learnsets.js

--- mods/gen6/learnsets.js
+++ mods/gen6/learnsets.js
@@ -1,1 +1,4 @@
-*/fdfsf
+exports.BattleLearnsets = {
+        yveltal:{learnset:{toxic:["5L0"],doubleedge:["5L0"],oblivionwing:["5L0"],darkpulse:["5L0"],airslash:["5L0"],hurricane:["5L0"],aurasphere:["5L0"],dragondance:["5L0"],focusblast:["5L0"],superpower:["5L0"],roost:["5L0"]}},
+        xerneas:{learnset:{toxic:["5L0"],doubleedge:["5L0"],geomancy:["5L0"],fairywind:["5L0"],gigadrain:["5L0"],leafstorm:["5L0"],aurasphere:["5L0"],lunardance:["5L0"],calmmind:["5L0"],powerwhip:["5L0"],recover:["5L0"]}},        
+};
