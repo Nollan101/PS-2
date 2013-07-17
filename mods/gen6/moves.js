213f77c172be20a7f5b18b02d28a41437675ab7b
Oiawesome <Oiawesome99@yahoo.com>
2013-07-16 19:13:33 -0700

    Update moves.js

--- mods/gen6/moves.js
+++ mods/gen6/moves.js
@@ -15,6 +15,26 @@ exports.BattleMovedex = {
                 target: "normal",
                 type: "Grass"
         },
+      	"lunardance": {
+		num: 461,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		desc: "The user faints and the Pokemon brought out to replace it has its HP and PP fully restored along with having any major status condition cured. Fails if the user is the last unfainted Pokemon in its party.",
+		shortDesc: "User faints. Replacement is fully healed, with PP.",
+		id: "lunardance",
+		isViable: true,
+		name: "Lunar Dance",
+		pp: 20,
+		priority: 0,
+		isSnatchable: true,
+		boosts: {
+			spa: 1,
+			spe: 1
+		},
+		target: "self",
+		type: "Psychic"
+	},         
 	"moonblast": {
 		num: 1000,
 		accuracy: 90,
