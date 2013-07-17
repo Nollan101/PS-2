7b5bc0dcfaa198fa7634e87218d131861e0de425
Oiawesome <Oiawesome99@yahoo.com>
2013-07-17 14:07:36 -0700

    Create pokedex.js

---
+++ mods/gen6/pokedex.js
@@ -0,0 +1,3 @@
+exports.BattlePokedex = {
+charizard:{num:6,species:"Charizard",types:["Fire","Dragon"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:78,atk:84,def:78,spa:109,spd:85,spe:100},abilities:{0:"Blaze",DW:"Levitate"},heightm:1.7,weightkg:90.5,color:"Red",prevo:"charmeleon",evoLevel:36,eggGroups:["Monster","Dragon"]},
+};
