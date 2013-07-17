c8ff8e800546508746d8671f8c089b932eb2cdd3
Oiawesome <Oiawesome99@yahoo.com>
2013-07-17 14:19:18 -0700

    Update pokedex.js

--- mods/gen6/pokedex.js
+++ mods/gen6/pokedex.js
@@ -1,3 +1,4 @@
 exports.BattlePokedex = {
-charizard:{num:6,species:"Charizard",types:["Fire","Dragon"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:78,atk:84,def:78,spa:109,spd:85,spe:100},abilities:{0:"Blaze",DW:"Levitate"},heightm:1.7,weightkg:90.5,color:"Red",prevo:"charmeleon",evoLevel:36,eggGroups:["Monster","Dragon"]},
+yveltal:{num:800,species:"Yveltal",types:["Flying","Dark"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:95,atk:100,def:95,spa:135,spd:100,spe:105},abilities:{0:"Dark Aura",1:"Moxie",DW:"Intimidate"},heightm:1.7,weightkg:90.5,color:"Red",eggGroups:["Monster","Dragon"]},
+xerneas:{num:801,species:"Xerneas",types:["Grass","Fairy"],baseStats:{hp:105,atk:95,def:100,spa:105,spd:135,spe:105},abilities:{0:"Fairy Aura",1:"Magic Guard",DW:"Natural Cure"},heightm:2,weightkg:120,color:"Grey",eggGroups:["Monster","Plant"]}
 };
