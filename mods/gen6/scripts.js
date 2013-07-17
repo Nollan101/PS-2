d8812213994776a87eff21584ac830b7b91045b9
Oiawesome <Oiawesome99@yahoo.com>
2013-07-16 19:09:27 -0700

    Update scripts.js

--- mods/gen6/scripts.js
+++ mods/gen6/scripts.js
@@ -14,8 +14,15 @@ exports.BattleScripts = {
         this.data.Learnsets.gardevoir.learnset.drainingkiss = ['5L0'];        
 
         this.data.Pokedex.clefable.types = ["Normal","Fairy"];
-    
+        this.data.Learnsets.clefable.learnset.fairywind = ['5L0'];
+        this.data.Learnsets.clefable.learnset.lunardance = ['5L0'];
+        this.data.Learnsets.clefable.learnset.topsyturvey = ['5L0'];     
         
         this.data.Pokedex.gallade.types = ["Fighting","Fairy"];
+        this.data.Learnsets.gallade.learnset.fairywind = ['5L0'];
+        this.data.Learnsets.gallade.learnset.lunardance = ['5L0'];
+        this.data.Learnsets.gallade.learnset.drainingkiss = ['5L0'];              
+        this.data.Learnsets.gallade.learnset.lunardance = ['5L0'];
+        this.data.Learnsets.gallade.learnset.mysticcrash = ['5L0'];        
              
 };
